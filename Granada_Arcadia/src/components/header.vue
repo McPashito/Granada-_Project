2738312f8650e8646674b111a227d4f6c41b1ee2
McPashito <alvaroromo1985@gmail.com>
2026-03-17 15:22:59 +0100

    Dia Martes 17/03 MAÑANA

---
+++ Granada_Arcadia/src/components/header.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <header class="main-header">
+    <div class="logo"></div>
+
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Inicio</router-link>
+        </li>
+
+        <li>
+          <router-link to="/collection">Colecciones</router-link>
+        </li>
+
+        <li>
+          <router-link to="/record">Discos</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ Granada_Arcadia/src/router/index.js
@@ -0,0 +1,37 @@
+import RecordView from '@/views/RecordView.vue'
+import CollectionView from '@/views/CollectionView.vue'
+import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/collection', // 1. La URL
+      name: 'collection', // 2. Un nombre identificador
+      component: CollectionView, // 3. El componente Vue que se muestra
+    },
+    {
+      path: '/collection/:id', // 1. La URL
+      name: 'collectionid', // 2. Un nombre identificador
+      component: CollectionView, // 3. El componente Vue que se muestra
+    },
+    {
+      path: '/record', // 1. La URL
+      name: 'record', // 2. Un nombre identificador
+      component: RecordView, // 3. El componente Vue que se muestra
+    },
+    {
+      path: '/record/:id', // 1. La URL
+      name: 'recordid', // 2. Un nombre identificador
+      component: RecordView, // 3. El componente Vue que se muestra
+    },
+    {
+      path: '/', // 1. La URL
+      name: 'Home', // 2. Un nombre identificador
+      component: HomeView, // 3. El componente Vue que se muestra
+    },
+  ],
+})
+
+export default router
