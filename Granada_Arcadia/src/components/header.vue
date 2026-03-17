de3b38668c193c0657e31b4cf52c4d0d9715bfc1
McPashito <alvaroromo1985@gmail.com>
2026-03-17 16:47:39 +0100

    Martes 17/03/Tarde

--- Granada_Arcadia/src/components/header.vue
+++ Granada_Arcadia/src/components/header.vue
@@ -2,22 +2,67 @@
 
 <template>
   <header class="main-header">
-    <div class="logo"></div>
+    <div class="nav-bg">
+      <nav class="nav-footer">
+        <ul>
+          <li>
+            <router-link :to="{ name: 'Home' }">Inicio</router-link>
+          </li>
 
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Inicio</router-link>
-        </li>
+          <li>
+            <router-link :to="{ name: 'collection' }">Colecciones</router-link>
+          </li>
 
-        <li>
-          <router-link to="/collection">Colecciones</router-link>
-        </li>
-
-        <li>
-          <router-link to="/record">Discos</router-link>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <router-link :to="{ name: 'record' }">Discos</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
+
+<style scoped>
+:root {
+  --blanco: #ffffff;
+  --oscuro: #212121;
+  --primario: #ffc107;
+  --secundario: #0097a7;
+  --gris: #757575;
+  --GrisClaro: #dfe9f3;
+}
+
+.main-header {
+  background-color: #dfe9f3;
+}
+.nav-bg {
+  margin: 2rem 2rem 2rem 2rem;
+  display: flex;
+  flex-direction: row;
+  grid-row: 1 / 2;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: #0097a7;
+  color: #ffffff;
+  border-radius: 0.8rem;
+}
+.nav-footer ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  list-style-type: none;
+  gap: 20rem;
+}
+.nav-footer a {
+  text-decoration: none;
+  color: var(--blanco);
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+.nav-footer a:hover {
+  text-decoration: underline;
+  color: #ffc107;
+}
+</style>
---
+++ Granada_Arcadia/src/views/RecordView.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>NombreDelArchivo</div>
+</template>
