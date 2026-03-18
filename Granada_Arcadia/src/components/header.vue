dfa076c2b600d6723909de0368535527b6db42c4
McPashito <alvaroromo1985@gmail.com>
2026-03-18 12:56:31 +0100

    18/03 HEADER

--- Granada_Arcadia/src/components/header.vue
+++ Granada_Arcadia/src/components/header.vue
@@ -3,6 +3,7 @@
 <template>
   <header class="main-header">
     <div class="nav-bg">
+      <div class="nav-logo"></div>
       <nav class="nav-footer">
         <ul>
           <li>
@@ -18,6 +19,7 @@
           </li>
         </ul>
       </nav>
+      <nav class="nav-logo"></nav>
     </div>
   </header>
 </template>
@@ -34,33 +36,41 @@
 
 .main-header {
   background-color: #dfe9f3;
+  height: 7.2rem;
 }
+
 .nav-bg {
-  background-color: #0097a7;
-  margin: 2rem 2rem 2rem 2rem;
-  display: flex;
-  flex-direction: row;
-  grid-row: 1 / 2;
   justify-content: space-between;
+  background-color: #0097a7;
+  margin: 0 auto;
+  height: 100%;
+  width: min(90rem, 90%);
+  display: flex;
   align-items: center;
-  padding: 20px;
+  padding: 0 2rem;
   font-family: 'Roboto', sans-serif;
   color: #ffffff;
-  border-radius: 0.8rem;
 }
+
+.nav-footer {
+  width: 100%;
+}
+
 .nav-footer ul {
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: center;
+  gap: 3rem;
   list-style-type: none;
-  gap: 20rem;
+  padding: 0;
+  margin: 0;
 }
+
 .nav-footer a {
   text-decoration: none;
-  color: var(--blanco);
-  font-size: 1.2rem;
+  color: #ffffff;
+  font-size: 2.2rem;
   font-weight: bold;
+  padding: 0.8rem;
 }
 .nav-footer a:hover {
   text-decoration: none;
@@ -68,6 +78,7 @@
   color: #212121;
   background-color: #ffc107;
   padding: 0.8rem;
+
   border-radius: 0.8rem;
 }
 </style>
