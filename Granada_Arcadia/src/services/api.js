19a3cad44c2e8dd7ed3984d4169bf312370c4865
McPashito <alvaroromo1985@gmail.com>
2026-03-20 14:16:46 +0100

    20/03 MAÑANA

--- Granada_Arcadia/src/services/api.js
+++ Granada_Arcadia/src/services/api.js
@@ -6,10 +6,21 @@ async function getCollections() {
   return datos.items
 }
 
-async function getRecords() {
-  const respuesta = await fetch(BASE_URL + '/record')
+async function getRecords(offset) {
+  const respuesta = await fetch(BASE_URL + `/record?limit=16&offset=${offset}`)
   const datos = await respuesta.json()
   return datos.items
 }
 
-export { getCollections, getRecords }
+async function getRecordById(id) {
+  const respuesta = await fetch(BASE_URL + `/record/${id}`)
+  const datos = await respuesta.json()
+  return datos
+}
+async function getCollectionById(id) {
+  const respuesta = await fetch(BASE_URL + `/collection/${id}`)
+  const datos = await respuesta.json()
+  return datos
+}
+
+export { getCollections, getRecords, getRecordById, getCollectionById }
--- Granada_Arcadia/src/views/RecordView.vue
+++ Granada_Arcadia/src/views/RecordView.vue
@@ -4,19 +4,22 @@ import { getRecords } from '../services/api.js'
 export default {
   data() {
     return {
+      offset: 0,
       records: [],
     }
   },
   async mounted() {
-    this.records = await getRecords()
+    this.records = await getRecords(this.offset)
   },
 }
 </script>
 
 <template>
   <section class="cuadro">
-    <div class="enlace" v-for="record in records" :key="record.name">
-      <h3>{{ record.metadata_fields['dcterms:title']?.[0]?.['@value'] }}</h3>
+    <div class="enlace" v-for="record in records" :key="record.id">
+      <router-link :to="{ name: 'recordid', params: { id: record.id } }">
+        <h3>{{ record.metadata_fields['dcterms:title']?.[0]?.['@value'] }}</h3>
+      </router-link>
     </div>
   </section>
 </template>
