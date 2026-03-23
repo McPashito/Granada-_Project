570bc66eec2d9b8145d6870e2d881ae795205a95
McPashito <alvaroromo1985@gmail.com>
2026-03-23 16:01:50 +0100

    Feat: Paginacion

--- Granada_Arcadia/src/views/RecordView.vue
+++ Granada_Arcadia/src/views/RecordView.vue
@@ -11,6 +11,18 @@ export default {
   async mounted() {
     this.records = await getRecords(this.offset)
   },
+  methods: {
+    async loadMore() {
+      this.offset += 16
+      this.records = await getRecords(this.offset)
+    },
+    async loadLess() {
+      if (this.offset >= 16) {
+        this.offset -= 16
+        this.records = await getRecords(this.offset)
+      }
+    },
+  },
 }
 </script>
 
@@ -21,5 +33,12 @@ export default {
         <h3>{{ record.metadata_fields['dcterms:title']?.[0]?.['@value'] }}</h3>
       </router-link>
     </div>
+  </section>
+  <section>
+    <li>
+      <button @click="loadLess" :disabled="offset < 16">Anterior</button>
+      <h6>Página {{ offset / 16 + 1 }}</h6>
+      <button @click="loadMore" :disabled="records.length < 16">Siguiente</button>
+    </li>
   </section>
 </template>
