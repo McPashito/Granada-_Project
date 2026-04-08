64f37a95f8f0013105234c1dc47f9be6e00b5f04
McPashito <alvaroromo1985@gmail.com>
2026-04-08 11:19:43 +0200

    feat: details ref

--- Granada_Arcadia/src/views/RecordDetailView.vue
+++ Granada_Arcadia/src/views/RecordDetailView.vue
@@ -12,33 +12,18 @@ export default {
 }
 </script>
 <template>
-  <section class="record-details">
-    <section class="details" v-if="record">
-      <div class="details-img">
-        <img
-          v-if="record.thumbnail"
-          :src="'https://arcadium.cluster24.libnamic.eu' + record.thumbnail"
-          alt=""
-        />
-      </div>
-      <div class="details-description">
-        <h2>Título: {{ record.title }}</h2>
-        <h2>Fecha: {{ record.date }}</h2>
-        <h3>Descripcion: {{ record.description }}</h3>
-      </div>
-    </section>
+  <section class="details" v-if="record">
+    <div class="details-img">
+      <img
+        v-if="record.thumbnail"
+        :src="'https://arcadium.cluster24.libnamic.eu' + record.thumbnail"
+        alt=""
+      />
+    </div>
+    <div class="details-description">
+      <h2>Título: {{ record.title }}</h2>
+      <h2>Fecha: {{ record.date }}</h2>
+      <h3>Descripcion: {{ record.description }}</h3>
+    </div>
   </section>
 </template>
-<style scoped>
-.record-details {
-  display: flex;
-  height: 60vh;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-.details {
-  width: 100%;
-  max-width: 1024px;
-}
-</style>
